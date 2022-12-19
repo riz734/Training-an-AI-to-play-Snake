6bf9cafcacc91e3c4670f6df92e32752dbd579a2
Shahbab Ahmed <shahbaba@umich.edu>
2022-12-19 01:43:07 -0500

    setting ai

---
+++ agent.py
@@ -0,0 +1,39 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from snake_game import SnakeGameAI, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+
+    def __init__(self):
+        self.n_game = 0
+        self.epsilon = 0
+
+    def get_state(self,game):
+        pass
+
+    def remember(self,state,action,reward,next_state,game_over):
+        pass
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self.state):
+        pass
+
+def train():
+    pass
+
+
+if __name__ == "__main__":
+    train()
+
+
