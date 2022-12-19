6a867bf9b64eaa08636fbf290ee18bb29952fb6a
Shahbab Ahmed <shahbaba@umich.edu>
2022-12-19 02:02:14 -0500

    added state logic

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ import torch
 import random
 import numpy as np
 from collections import deque
-from snake_game import SnakeGameAI, Direction, Point
+from game import SnakeGameAI, Direction, Point
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -11,26 +11,116 @@ LR = 0.001
 class Agent:
 
     def __init__(self):
-        self.n_game = 0
-        self.epsilon = 0
+        self.n_games = 0
+        self.epsilon = 0 #randomness
+        self.gamma = 0 #discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = None #TODO
+        self.trainer = None #TODO
+        
+        #TODO: mode, trainer
+
 
     def get_state(self,game):
-        pass
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+        
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
 
-    def remember(self,state,action,reward,next_state,game_over):
-        pass
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+            
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            # Food location 
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y  # food down
+            ]
+
+        return np.array(state, dtype=int)
+
+    def remember(self,state,action,reward,next_state,done):
+        self.memory.append(state,action,reward,next_state,done) #popleft if max memory is reached
 
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self,state,action,reward,next_state,done):
         pass
 
     def get_action(self.state):
         pass
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_scores = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+
+    while True:
+        #get old state
+
+        state_old = agent.get_state(game)
+
+        #get move
+        final_move = agent.get_action(state_old)
+
+        #perform move and get new state
+        reward,done,score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        #train short memory
+        agent.train_short_memory(state_old,final_move,reward,state_new,done)
+        
+        #remember
+        agent.remember(state_old,final_move,reward,state_new,done)
+
+        if done:
+            #train long memory, plot result
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+
+                #agent.mode.save()
+
+            print("Game",agent.n_games, "Score", score, "Record:",record)
+
+            #TODO: plot
+
+
 
 
 if __name__ == "__main__":
